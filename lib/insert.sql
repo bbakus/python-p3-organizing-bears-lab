b856e2b08411eb1c1864eb868fd980deb9548f65
Brandon Bakus <brandonbakus@gmail.com>
2025-02-28 12:45:47 -0500

    BEARS

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 10, 'M', 'Brown', 'Shady', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Rowdy', 69, 'F', 'White', 'Sassy', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'Blue', 'Salty', 0 );
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'Red', 'Sweet', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Melissa', 15, 'F', 'Black', 'Heavy', 0);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'Black', 'Goofy', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'Purple', 'Happy', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES (NULL, 5, 'F', 'Purple', 'Happy', 1);
+
